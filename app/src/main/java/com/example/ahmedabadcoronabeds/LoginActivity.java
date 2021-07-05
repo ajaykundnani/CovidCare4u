165b4a482f10b6fe253cfd139fd8315fbef97fe1
yashpaida <iamyashpaida@gmail.com>
2021-07-05 18:12:50 +0530

    Login with Shared Preferences added

--- app/src/main/java/com/example/ahmedabadcoronabeds/LoginActivity.java
+++ app/src/main/java/com/example/ahmedabadcoronabeds/LoginActivity.java
@@ -1,19 +1,33 @@
 package com.example.ahmedabadcoronabeds;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ahmedabadcoronabeds.Models.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private DatabaseReference dataReference = FirebaseDatabase.getInstance().getReference().child("Users");
+    private List<User> users = new ArrayList<>();
+    private  SharedPreferences sharedPreferences;
     EditText entered_captcha, mobile, pass;
     TextView captcha;
 
@@ -28,8 +42,50 @@ public class LoginActivity extends AppCompatActivity {
         pass = (EditText)findViewById(R.id.pass);
 
         Captcha randomString = new Captcha();
-        String result = randomString.generateAlphaNumeric(8);
+        String result = randomString.generateAlphaNumeric(6);
         captcha.setText(result);
+
+        FetchUsers();
+    }
+
+    private void FetchUsers()
+    {
+        dataReference.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for (DataSnapshot HospitalSnapshot: snapshot.getChildren()) {
+                    User user = HospitalSnapshot.getValue(User.class);
+                    users.add(user);
+                }
+                //Bind Method Calling
+                System.out.println(users);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("Error",error.getMessage());
+            }
+        });
+    }
+
+    private boolean IsValidUser(String MobileNo, String Password){
+        if(users.size() != 0){
+            for (User u : users) {
+                if (u.getMobileNo().equals(MobileNo) && u.getPassword().equals(Password)) {
+                    sharedPreferences = getSharedPreferences("User",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("name",u.getName());
+                    editor.putString("role",u.getRole());
+                    editor.putString("mobileno",u.getMobileNo());
+                    editor.putString("hospitalCode",u.getHospitalCode());
+                    editor.putString("password",u.getPassword());
+                    editor.putString("age",u.getAge());
+                    editor.apply();
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public void Refresh(View view){
@@ -38,23 +94,26 @@ public class LoginActivity extends AppCompatActivity {
         captcha.setText(result);
     }
     public void Login(View view){
-        if(mobile.getText().toString().equals("0123")&&pass.getText().toString().equals("456")&&
-                entered_captcha.getText().toString().equals(captcha.getText().toString())) {
-
-            Toast.makeText(getApplicationContext(),"login success",Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(LoginActivity.this,ManageActivity.class);
-            startActivity(i);
+        if(entered_captcha.getText().toString().equals(captcha.getText().toString())) {
+            if (IsValidUser(mobile.getText().toString(), pass.getText().toString())) {
+                Toast.makeText(getApplicationContext(), "Welcome "+sharedPreferences.getString("role",""), Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(i);
+            } else {
+                Toast.makeText(getApplicationContext(), "Invalid Credentials", Toast.LENGTH_SHORT).show();
+            }
         }
         else{
-            Toast.makeText(getApplicationContext(),"invalid login",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Invalid Captcha", Toast.LENGTH_SHORT).show();
         }
     }
 }
 
+
 class Captcha {
     private String letters ="abcdefghijklmnopqrstuvwxyz";
     private String number ="0123456789";
-    private String characters ="'@','_','-','&'";
+    private String characters ="@&_-";
     private char[] alphanumeric =(letters + letters.toUpperCase()+ number + characters).toCharArray();
 
     public String generateAlphaNumeric(int length){
